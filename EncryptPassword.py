43b368d65714bcbdeb5b8d4b607ebdd4fa5e559a
shan83kalai <120541935+shan83kalai@users.noreply.github.com>
2025-01-04 23:05:25 +0000

    Update EncryptPassword.py

--- EncryptPassword.py
+++ EncryptPassword.py
@@ -6,7 +6,6 @@ print(f"Secret key (save securely, e.g., environment variables): {key.decode()}"
 
 # Encrypt the password
 fernet = Fernet(key)
-# password = "your_actual_password_here"
-password = "Register100$"
+password = "your_actual_password_here"
 encrypted_password = fernet.encrypt(password.encode())
 print(f"Encrypted Password: {encrypted_password.decode()}")
